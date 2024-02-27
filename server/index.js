005077225219024aba46f0e73bf567c9427fcbb2
Edna Gomez Pallares <ednagomezpallares@Ednas-MBP.lan>
2024-02-26 21:10:01 -0500

    first commit, brain storming

--- server/index.js
+++ server/index.js
@@ -1,2 +1,14 @@
+//User
+//Email PK (Pass Key)
+//Password (string varchar)
+//First Name (string varchar)
+//Last Name (string varchar)
+//Address (string varchar)
+//Payment Info (string varchar)
+//Phone Number (string varchar)
+//Logged In (boolean)
+//Admin? (boolean)
 
-
+//SELECT * FROM users
+//SELECT * FROM users WHERE admin == true
+//SELECT * FROM users WHERE email == "bob@email.com"
